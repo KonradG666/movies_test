12fb6470ca75c97234794fe4aeb3fd2bc2563d14
Konrad Gawron <konrad.gawron@poczta.fm>
2021-01-26 17:00:08 +0100

    fix bugs and add tests

--- movies_catalogue/tests/test_main.py
+++ movies_catalogue/tests/test_main.py
@@ -1,25 +1,15 @@
+from main import app
 from unittest.mock import Mock
 import pytest
-from main import app
 
-@pytest.mark.parametrize("list_type, movie",
-                       [ ("/" ,"movie/popular"),
-                        ("/?list_type=popular", "movie/popular"),
-                        ("/?list_type=top_rated","movie/top_rated"),
-                        ("/?list_type=upcoming","movie/upcoming"),
-                        ("/?list_type=now_playing","movie/now_playing"),
-                        ("/?list_type=nowa", "movie/popular")])
-
-def test_movie_list(monkeypatch, list_type, movie):
-    print(list_type)
-    print(movie)
-    api_mock = Mock(return_value={'results': []})
+@pytest.mark.parametrize("list_type", ("top_rated", "upcoming", "popular", "now_playing"))
+def test_movies_list(monkeypatch, list_type):
+    api_mock = Mock(return_value={"results": []})
     monkeypatch.setattr("tmdb_client.call_tmdb", api_mock)
 
     with app.test_client() as client:
-       response = client.get(list_type)
-       assert response.status_code == 200
-       api_mock.assert_called_once_with(movie)
+        response = client.get("/")
+        assert response.status_code == 200
 
 
 
