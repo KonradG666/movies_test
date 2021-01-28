1240d7091ac9b60b328a0c57ee0b895ab0b768af
Konrad Gawron <konrad.gawron@poczta.fm>
2021-01-28 09:40:54 +0100

    remove token

--- movies_catalogue/tests/test_main.py
+++ movies_catalogue/tests/test_main.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 import pytest
 
 @pytest.mark.parametrize("list_type", ("popular", "top_rated", "upcoming", "now_playing"))
-def test_movies_list(monkeypatch, list_type):
+def test_hompepage(monkeypatch, list_type):
     api_mock = Mock(return_value={"results": []})
     monkeypatch.setattr("tmdb_client.call_tmdb", api_mock)
 
@@ -11,5 +11,3 @@ def test_movies_list(monkeypatch, list_type):
         response = client.get("/")
         assert response.status_code == 200
         api_mock.assert_called_once_with("movie/popular")
-
-
